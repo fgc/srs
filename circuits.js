c178f20c96ddbfc6adfe99d4dacf0f20dcd3ef62
Juan Felipe García Catalán <jfgcatalan@gmail.com>
2013-09-22 22:55:01 +0200

    Return signals from likely functions. Lifting

--- circuits.js
+++ circuits.js
@@ -3,11 +3,11 @@ function Signal() {
     this.action_procedures = new Array();
 }
 
-Signal.prototype.get = function() {
+Signal.prototype.get_value = function() {
     return this.value;
 };
 
-Signal.prototype.set = function(new_value) {
+Signal.prototype.set_value = function(new_value) {
     if (this.value != new_value) {
         this.value = new_value;
         for (var i = 0; i < this.action_procedures.length; i++) {
@@ -41,9 +41,9 @@ function propagate() {
 
 function AndGate(input_a, input_b, output) {
     function and_action_proc() {
-        var new_value = input_a.get() && input_b.get();
+        var new_value = input_a.get_value() && input_b.get_value();
         after_delay(function() {
-            output.set(new_value);
+            output.set_value(new_value);
         }, 1);
     }
 
@@ -55,13 +55,13 @@ function Ram(address, data, write) {
     var contents = new Array();
     function read_proc() {
         after_delay(function() {
-            data.set(contents[address.get()]);
+            data.set_value(contents[address.get_value()]);
         } , 1);
     }
 
     function write_proc() {
         after_delay(function() {
-            contents[address.get()] = data.get();
+            contents[address.get_value()] = data.get_value();
         } , 1);
     }
 
@@ -72,31 +72,43 @@ function Ram(address, data, write) {
 function Clock(delay, output) {
     output.add_action(function() {
         after_delay( function() {
-            output.set(!output.get());
-            console.log("Clock: " + output.get());
+            output.set_value(!output.get_value());
+            console.log("Clock: " + output.get_value());
             }, delay);
     });
 }
 
-function Text(id, signal) {
+function Text(id, input) {
     var text_element = document.createElement("span");
     text_element.id = id;
     document.body.appendChild(text_element);
-    signal.add_action(function() {
-        text_element.innerHTML = signal.get();
-    });
-}
-
-
-function Mouse(output) {
-    output.set({x: undefined, y: undefined});
-    document.addEventListener("mousemove", function(e){
-        output.set({x: e.pageX, y: e.pageY});
-    },false);
-}
-
-function Stringify(input, output) {
     input.add_action(function() {
-        output.set(JSON.stringify(input.get()));
+        text_element.innerHTML = input.get_value();
     });
+}
+
+
+function Mouse() {
+    var output = new Signal();
+    output.set_value({x: undefined, y: undefined});
+    document.addEventListener("mousemove", function(e){
+        output.set_value({x: e.pageX, y: e.pageY});
+    },false);
+    return output;
+}
+
+function Stringify(input) {
+    var output = new Signal();
+    input.add_action(function() {
+        output.set_value(JSON.stringify(input.get_value()));
+    });
+    return output;
+}
+
+function lift(proc, input) {
+    var output = new Signal();
+    input.add_action(function() {
+        output.set_value(proc(input.get_value()));
+    });
+    return output;
 }
