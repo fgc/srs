b500776f7772e6af310ad553ff9b53ae439304e8
Juan Felipe García Catalán <jfgcatalan@gmail.com>
2013-10-08 11:52:36 +0200

    sample_on and example

--- srs_event.js
+++ srs_event.js
@@ -2,7 +2,6 @@
 var SRS = (function(srs) {
 
     srs.rising_edge = function (signal, output) {
-        var output = output || new srs.Event();
 
         var rising = function (signal_value) {
             if (signal_value) {
@@ -16,6 +15,21 @@ var SRS = (function(srs) {
 
     srs.Signal.prototype.rising_edge = function(output) {
         return srs.rising_edge(this, output);
+    };
+
+
+    srs.sample_on = function (signal, event, output) {
+        var output = output || new srs.Event();
+
+        var sample = function() {
+            return signal.get_value();
+        }
+
+        return srs.lift(sample, event, 1, output);
+    };
+
+    srs.Signal.prototype.sample_on = function(event, output) {
+        return srs.sample_on(this, event, output);
     };
 
     return srs;
