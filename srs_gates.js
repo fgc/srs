68d387ac8445a10ced8d81ff2f238ed7bdfb0bde
Juan Felipe García Catalán <jfgcatalan@gmail.com>
2013-10-09 19:36:41 +0200

    Convert between number and array of bit buses

--- srs_gates.js
+++ srs_gates.js
@@ -28,6 +28,41 @@ var SRS = (function(srs) {
     };
 
 
+    /* Convert between buses as number signals and arrays of single bit signals*/
+    srs.to_bus = function (width, input) {
+        var bits = Array.apply(null, new Array(width)).map(function(){
+            return new SRS.Signal();
+        });
+        
+        input.lift(function(n) {
+            bits.map(function(bit,i){
+                bit.set_value((n >> i) & 1);
+            });
+        },1);
+
+        return bits;
+    };
+
+    srs.Signal.prototype.to_bus = function(width) {
+        return srs.to_bus(width, this);
+    };
+    
+    srs.from_bus = function(bus, output) {
+        var output = output || new srs.Signal(); //Don't remove this one, even if you
+                                                 //clean it from other places
+        var bus_to_number = function() {
+            return bus.reduce(function (acc, bit, i) {
+                console.log(i + "->" + (bit.get_value() << i));
+                return acc + (bit.get_value() << i);
+            },0);
+        };
+
+        bus.map(function(s){s.lift(bus_to_number,1,output);});
+        
+        return output;
+    };
+
+
     /** end logic gates **/
     return srs;
 }(SRS));
