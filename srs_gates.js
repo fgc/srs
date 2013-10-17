43019c4732ce4f31bd7796ea5bb4260b681bcf10
Juan Felipe García Catalán <jfgcatalan@gmail.com>
2013-10-17 10:13:32 +0200

    Better from_bus

--- srs_gates.js
+++ srs_gates.js
@@ -36,8 +36,8 @@ var SRS = (function(srs) {
         
         input.lift(function(n) {
             bits.map(function(bit,i){
-                //this might need 2-complement conversion
-                bit.set_value((n >> i) & 1);
+                //think/test about 2-complement
+                bit.set_value((n >>> i) & 1);
             });
         },1);
 
@@ -53,22 +53,19 @@ var SRS = (function(srs) {
                                                  //clean it from other places we do need
                                                  //a real output to connect to every bit
 
-        //This is quite bad, lots of unneeded recalcs, might be useful to
-        //have a current value and deal with incoming single bits.
-
-        /* get the current values for every bit signal in the bus
-           and return the corresponding number */
-        var bus_to_number = function() {
-            return bus.reduce(function (acc, bit, i) {
-                //console.log(i + "->" + (bit.get_value() << i));
-                //2-complement missing!
+        /* get the initial values for every bit signal in the bus
+           and store the corresponding number */
+        var value = bus.reduce(function (acc, bit, i) {
                 return acc + (bit.get_value() << i);
             },0);
+
+        var bus_to_number = function(i) {
+            return function() {return value | (1 << i)};
         };
 
         /* recalculate the output for every change of one of the signals
            in the bus*/
-        bus.map(function(s){s.lift(bus_to_number,1,output);});
+        bus.map(function(s,i){s.lift(bus_to_number(i),1,output);});
         
         return output;
     };
